c8a164d9a8259011a29a15b07af616e3f0857059
Melissa Ferrari <melissa.ferrari.v@gmail.com>
2024-08-18 13:46:07 -0300

    se trabajo sobre componente Menu

---
+++ src/Menu.jsx
@@ -0,0 +1,19 @@
+import './Menu.css'
+
+const Menu = () => {
+    return (
+      <div className="menu-container">
+        <div className="logo">
+          <img src="ruta/a/tu/logo.png" alt="Logo" className="logo-img" />
+        </div>
+        <ul className="menu-list">
+          <li className="menu-item">Búsqueda y reserva de libros</li>
+          <li className="menu-item">Préstamos de clase</li>
+          <li className="menu-item">Recomendaciones y listas de lectura</li>
+          <li className="menu-item">Interacción con Bibliotecario</li>
+        </ul>
+      </div>
+    );
+  };
+
+  export default Menu;
