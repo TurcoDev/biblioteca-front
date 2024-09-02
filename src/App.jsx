7c452dbbd555960d6d38bbe8a9ab9c901e38071d
andreazunino <108498948+andreazunino@users.noreply.github.com>
2024-09-02 19:53:07 -0300

    cambios andrea y melisa

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
+import Header from './Header.jsx';
+import Footer from './Footer.jsx';
 import Ingresar from './Ingresar.jsx';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import IniciarSesion from './IniciarSesion.jsx';
 import './App.css'
+import './PerfilAdulto.css';
 
-function App() {
+import DropdownMenu from './PerfilAdulto.jsx';
+
+/*function App() {
 
   return (
     <div className='contenedorPrincipal'>
+      <Header />
       <h1>Te damos la bienvenida!</h1>
       <Router>
       <Routes>
@@ -18,5 +24,28 @@ function App() {
     </div>
   )
 }
+*/
 
-export default App
+function App() {
+
+  return (
+    <div className='contenedorPrincipal'>
+      <Header />
+      <Footer />
+      <h1>Te damos la bienvenida!</h1>
+      <div>
+          <h2>Trabajo de Andrea</h2>
+          <DropdownMenu />
+      </div>
+      <Router>
+      <Routes>
+        <Route path="/" element={<Ingresar />} />
+        <Route path="/iniciar-sesion" element={<IniciarSesion />} />
+      </Routes>
+    </Router>
+    </div>
+    
+  )
+}
+
+export default App;
