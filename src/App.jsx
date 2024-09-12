4d42bd45d8e918aa3cad2c34e7a194f07f8e3387
VCIFARELLI <virginiacifarelli2018@gmail.com>
2024-09-12 19:38:43 -0300

    acomodar app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-<<<<<<< HEAD
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './Header';
+import Header from './Header.jsx';
+import Footer from './Footer.jsx';
+import Ingresar from './Ingresar.jsx';
 import Bienvenida from './Bienvenida';
 import EleccionGrado from "./EleccionGrado";
 import NumeroSeleccionado from "./NumeroSeleccionado";
@@ -9,6 +10,7 @@ import IniciarSesion from './IniciarSesion';
 import RegistroUsuario from './RegistroUsuario';
 import TarjetaDeIngreso from './IniciarSesionEst';
 import CrearSesionMayores from './IniciarSesionMayores';
+import DropdownMenu from './PerfilAdulto.jsx';
 
 import './App.css';
 
@@ -18,9 +20,15 @@ function App() {
         <Router>
           <div className='contenedorPrincipal'>
             <Header />
-            
+              <h1>Te damos la bienvenida!</h1>
+              <div>
+                <h2>Trabajo de Andrea</h2>
+                <DropdownMenu />
+              </div>
           <Routes>
           <Route path="/" element={<Bienvenida />} />
+          <Route path="/Footer" element={<Footer />} />
+          <Route path="/Ingresar" element={<Ingresar />} />
           <Route path="/elecciongrado" element={<EleccionGrado />} />
           <Route path="/numero/:number/:color" element={<NumeroSeleccionado />} />
           <Route path="/iniciarsesion" element={<IniciarSesion />} />
@@ -28,58 +36,8 @@ function App() {
           <Route path="/tarjetadeingreso" element={<TarjetaDeIngreso />} />
           <Route path="/CrearSesionMayores" element={<CrearSesionMayores />} />
         </Routes>
-      </div>
-=======
-import Header from './Header.jsx';
-import Footer from './Footer.jsx';
-import Ingresar from './Ingresar.jsx';
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import IniciarSesion from './IniciarSesion.jsx';
-import './App.css'
-import './PerfilAdulto.css';
-
-import DropdownMenu from './PerfilAdulto.jsx';
-
-/*function App() {
-
-  return (
-    <div className='contenedorPrincipal'>
-      <Header />
-      <h1>Te damos la bienvenida!</h1>
-      <Router>
-      <Routes>
-        <Route path="/" element={<Ingresar />} />
-        <Route path="/iniciar-sesion" element={<IniciarSesion />} />
-      </Routes>
->>>>>>> andrea
-    </Router>
-  );
+        </div>
+        </Router> 
+);
 }
-
-/*<<<<<<< HEAD
-=======
-function App() {
-
-  return (
-    <div className='contenedorPrincipal'>
-      <Header />
-      <Footer />
-      <h1>Te damos la bienvenida!</h1>
-      <div>
-          <h2>Trabajo de Andrea</h2>
-          <DropdownMenu />
-      </div>
-      <Router>
-      <Routes>
-        <Route path="/" element={<Ingresar />} />
-        <Route path="/iniciar-sesion" element={<IniciarSesion />} />
-      </Routes>
-    </Router>
-    </div>
-    
-  )
-}
-
->>>>>>> andrea*/
 export default App;
