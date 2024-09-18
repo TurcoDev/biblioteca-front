d7fb8cc272f6f15846ce042d76690cf208892dbb
vallejos2003 <flor.85vallejos@gmail.com>
2024-09-18 19:28:16 -0300

    opcionesbuscador

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,24 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import IniciarSesion from './IniciarSesion.jsx';
 import './App.css'
+import SearchBar from './Buscador.jsx';
+import LibraryButtons from './tresbotones.jsx';
 
-function App() {
-
+function App(){
   return (
     <div className='contenedorPrincipal'>
        <Header />
+       <LibraryButtons />
       <h1>Te damos la bienvenida!</h1>
       <Router>
       <Routes>
-        <Route path="/" element={<Ingresar />} />
-        <Route path="/iniciarsesion" element={<IniciarSesion />} />
+       {/* <Route path="/" element={<Ingresar />} /> 
+        <Route path="/iniciarsesion" element={<IniciarSesion />} /> */}
+         <Route path="/Tresopciones" element={<LibraryButtons />} />
       </Routes>
-    </Router>
+      </Router>
     </div>
   )
 }
 
-export default App
+export default App;
---
+++ src/tresbotones.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './tresbotones.css'; // Asegúrate de que este archivo tenga los estilos adecuados
+import BotonIngresar from './BotonIngresar.jsx';
+
+const LibraryButtons = () => {
+  return (
+    <div className="library-container">
+      <div className="button-container">
+        <div className="library-button">
+          <h3>Mis Préstamos</h3>
+          <BotonIngresar texto="Ver Prestamos" type="submit"/>
+        </div>
+        <div className="library-button">
+          <h3>Ver Todos los Libros</h3>
+          <BotonIngresar texto="Ver Libros" type="submit"/>
+        </div>
+        <div className="library-button">
+          <h3>Mis Recomendaciones</h3>
+          <BotonIngresar texto="Ver Recomendaciones" type="submit"/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LibraryButtons;
