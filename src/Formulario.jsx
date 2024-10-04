431412bf7555a3ff988d980e29aec2554edca313
Veronica <acupao271@gmail.com>
2024-10-03 21:31:50 -0300

    arreglo funciones y estilos

--- src/Formulario.jsx
+++ src/Formulario.jsx
@@ -1,50 +1,28 @@
-// Formulario.js
 import React from 'react';
 import BotonIngresar from './BotonIngresar'; // Asegúrate de que la ruta sea correcta
 import './Formulario.css'; // Importa el archivo CSS
 
-const Formulario = ({ tipo, textoBoton }) => {
+const Formulario = ({ campos, textoBoton }) => {
   return (
-    <div className="formulario-contenedor"> {/* Nuevo contenedor para aplicar estilos */}
-     
+    <div className="formulario-contenedor">
       <form className="formulario">
-        <h2>¡Creá tu cuenta!</h2> {/* Título común para ambos formularios */}
+        <h2>¡Ingresá los datos!</h2> {/* Título común para ambos formularios */}
 
-        <div>
-        {tipo === 'usuario' ? (
-          <>
-            <div>
-              <label htmlFor="usuario">Nombre de Usuario:</label>
-              <input type="text" id="usuario" name="usuario" />
-            </div>
-          </>
-        ) : (
-          <>
-            <div>
-              <label htmlFor="correo">Correo Electrónico:</label>
-              <input type="email" id="correo" name="correo" />
-            </div>
-          </>
-        )}
-        
-          <label htmlFor="nombre">Nombre:</label>
-          <input type="text" id="nombre" name="nombre" />
-        </div>
-        
-        <div>
-          <label htmlFor="apellido">Apellido:</label>
-          <input type="text" id="apellido" name="apellido" />
-        </div>
-        
-    
-        <div>
-          <label htmlFor="contraseña">Contraseña:</label>
-          <input type="password" id="contraseña" name="contraseña" />
-        </div>
+        {/* Generar dinámicamente los campos del formulario */}
+        {campos.map((campo, index) => (
+          <div key={index}>
+            <label htmlFor={campo.id}>{campo.label}:</label>
+            <input
+              type={campo.type}
+              id={campo.id}
+              name={campo.name}
+              placeholder={campo.placeholder || ''}
+            />
+          </div>
+        ))}
 
-        <BotonIngresar texto={textoBoton}  /> {/* Incluir el botón con el texto proporcionado */}
+        <BotonIngresar texto={textoBoton} /> {/* Incluir el botón con el texto proporcionado */}
       </form>
-      
     </div>
   );
 };
