4505b187ef12e4fc404e9113aab64bee6dab7d2a
Lucas Salvatori <lsalvatori.hms@gmail.com>
2024-10-04 22:48:21 -0300

    Se separa la lista de libros archivo independiente

---
+++ src/mocks/books.js
@@ -0,0 +1,8 @@
+// Simulación de datos (en lugar de una API)
+export const books = [
+  { id: 1, titulo: 'Cien Años de Soledad', autor: 'Gabriel García Márquez', isbn: 'Codigo1', año_publicacion: '1990', imagen: 'https://images.cdn3.buscalibre.com/fit-in/360x360/61/8d/618d227e8967274cd9589a549adff52d.jpg', descripcion: 'Descripción del libro.' },
+  { id: 2, titulo: 'Don Quijote de la Mancha', autor: 'Miguel de Cervantes', isbn: 'Codigo2', año_publicacion: '1990', imagen: 'https://images.cdn1.buscalibre.com/fit-in/360x360/19/c7/19c70e689956601918101b09f0bb0b3c.jpg', descripcion: 'Descripción del libro.' },
+  { id: 3, titulo: 'La Odisea', autor: 'Homero', isbn: 'Codigo3', año_publicacion: '1990', imagen: 'https://images.cdn3.buscalibre.com/fit-in/360x360/c0/5a/c05ad53842227a1416e03310ee840934.jpg', descripcion: 'Descripción del libro.' },
+  { id: 4, titulo: '1984', autor: 'George Orwell', isbn: 'Codigo4', año_publicacion: '1990', imagen: 'https://images.cdn1.buscalibre.com/fit-in/360x360/b0/39/b039af065268818b7bd3b0e016f8db65.jpg', descripcion: 'Descripción del libro.' },
+  { id: 5, titulo: 'El Principito', autor: 'Antoine de Saint-Exupéry', isbn: 'Codigo5', año_publicacion: '1990', imagen: 'https://images.cdn3.buscalibre.com/fit-in/360x360/68/0e/680e4d2b23ce77fc5520e984aeb2b68e.jpg', descripcion: 'Descripción del libro.' }
+];
