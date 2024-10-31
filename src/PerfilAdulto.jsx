4be1f024e59e7a514c785a30344c8e5edfab1d82
acvidela <acvidela@gmail.com>
2024-10-31 16:20:06 -0300

    se agrega el botón de crear usuario adulto

--- src/PerfilAdulto.jsx
+++ src/PerfilAdulto.jsx
@@ -1,29 +1,40 @@
 import React, { useState } from 'react';
 import './PerfilAdulto.css';
+import DropdownButtonCrearAdulto from './CrearUsuarioAdulto';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons'; // Importa el ícono que uso
+import { FaKey } from "react-icons/fa"; // Usa react-icons para el icono de la llave
 
 const DropdownMenu = () => {
-    const [isOpen, setIsOpen] = useState(false); // Estado para controlar la apertura del menú desplegable
+    const [isOpen, setIsOpen] = useState(false);
 
     const toggleDropdown = () => {
-        setIsOpen(!isOpen); // Alterna el estado de abierto/cerrado
+        setIsOpen(!isOpen);
     };
 
     return (
+        <>
         <div className="dropdown">
-            <button className="dropdown-button" onClick={toggleDropdown}>
-                <FontAwesomeIcon icon={faUser} className="icon" /> {/* Añade el ícono antes del texto */}
-                ¿A que perfil desea ingresar?
-                <span className={`arrow ${isOpen ? 'up' : 'down'}`}></span> {/* Flecha cambia según el estado */}
-            </button>
-            {isOpen && (
-                <div className="dropdown-content">
-                    <a href="#opcion1">Docente</a>
-                    <a href="#opcion2">Administrador</a>
-                </div>
-            )}
-        </div>
+        <button className="dropdown-button" onClick={toggleDropdown}>
+            <FontAwesomeIcon icon={faUser} className="icon" />
+            ¿A qué perfil desea ingresar?
+            <span className={`arrow ${isOpen ? 'up' : 'down'}`}></span>
+        </button>
+        {isOpen && (
+            <div className="dropdown-content">
+                <a href="#opcion1">Docente</a>
+                <a href="#opcion2">Administrador</a>
+            </div>
+        )}
+    </div>
+    
+    <div className="dropdown">
+        <button className="dropdown-button">
+            <FaKey className="icon" />
+            ¿Quieres crear una cuenta?
+        </button>
+    </div>
+    </>
     );
 };
 
