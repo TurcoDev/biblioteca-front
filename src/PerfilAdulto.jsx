a35345bf0f77608cec1ec1be159b1db9fd5ca4f4
Andrea Zunino <andreab.zunino@gmail.com>
2024-11-05 10:09:51 -0300

    se acomodan estilos

--- src/PerfilAdulto.jsx
+++ src/PerfilAdulto.jsx
@@ -3,40 +3,46 @@ import { useNavigate } from 'react-router-dom';
 import './PerfilAdulto.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
-import { UserContext } from './context/UserContext'; // Asegúrate de que esté importado
+import { FaKey } from "react-icons/fa"; // Usa react-icons para el ícono de la llave
+import { UserContext } from './context/UserContext'; 
 
 const DropdownMenu = () => {
     const [isOpen, setIsOpen] = useState(false);
     const navigate = useNavigate();
-    const { setUser } = useContext(UserContext); // Accede al contexto de usuario
+    const { setUser } = useContext(UserContext);
 
     const toggleDropdown = () => {
         setIsOpen(!isOpen);
     };
 
     const handleSelection = (role) => {
-        setUser(null); // Limpia el estado del usuario para que no persista
-        setIsOpen(false); // Cierra el menú
-        const color = "DarkRed"; // Color para mayores
-        const number = "18"; // Mayores de 18
+        setUser(null);
+        setIsOpen(false);
+        const color = "DarkRed";
+        const number = "18";
         navigate(`/CrearSesionUsr/${number}/${encodeURIComponent(color)}/${role}`);
     };
 
     return (
-        <div className="dropdown">
-            <button className="dropdown-button" onClick={toggleDropdown}>
-                <FontAwesomeIcon icon={faUser} className="icon" />
-                ¿A qué perfil desea ingresar?
-                <span className={`arrow ${isOpen ? 'up' : 'down'}`}></span>
-            </button>
-            {isOpen && (
-                <div className="dropdown-content">
-                    <button onClick={() => handleSelection("docente")}>Docente</button>
-                    <button onClick={() => handleSelection("administrador")}>Administrador</button>
-                </div>
-            )}
-        </div>
+        <>
+            <div className="dropdown">
+                <button className="dropdown-button" onClick={toggleDropdown}>
+                    <FontAwesomeIcon icon={faUser} className="icon" />
+                    ¿A qué perfil desea ingresar?
+                    <span className={`arrow ${isOpen ? 'up' : 'down'}`}></span>
+                </button>
+                {isOpen && (
+                    <div className="dropdown-content">
+                        <button onClick={() => handleSelection("docente")}>Docente</button>
+                        <button onClick={() => handleSelection("administrador")}>Administrador</button>
+                    </div>
+                )}
+            </div>
+
+        
+        </>
     );
 };
 
 export default DropdownMenu;
+
