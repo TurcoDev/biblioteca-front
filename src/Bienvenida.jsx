7cb56f797c7cc705bfcc37a08439a05efde97302
Andrea Zunino <andreab.zunino@gmail.com>
2024-11-05 16:17:39 -0300

    se cambia mensaje

--- src/Bienvenida.jsx
+++ src/Bienvenida.jsx
@@ -4,7 +4,7 @@ import './Bienvenida.css'
 export default function Bienvenida(){
     return (
         <div className="contenedorBienvenida">
-            <h2 className="tituloBienvenida">Te damos la bienvenida</h2>
+            <h2 className="tituloBienvenida">TE DAMOS LA BIENVENIDA</h2>
             <div className="contenedorBotones">
                 <BotonSoy 
                 nombre="SOY ESTUDIANTE" color="orange" ruta="/elecciongrado" />
